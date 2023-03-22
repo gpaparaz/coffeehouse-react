a01105a3b68f2519fc06a7486a2176ae7aebfb2f
gpaparaz <giorgia.paparazzo@gmail.com>
2023-03-22 14:13:47 +0100

    started calculate product items

--- coffeehouse/src/pages/home.js
+++ coffeehouse/src/pages/home.js
@@ -1,26 +1,37 @@
 import React, { Component } from "react";
 import HeroHome from "../components/hero-home/hero-home";
 import ProductCatalogueCard from "../components/product-catalogue/product-catalogue-card";
+import { collectionOfProducts } from "../assets/data/data";
+import _, { map } from 'underscore';
 
-class Home extends React.Component {
-
-    constructor(props) {
-        super(props)
-        this.state = { data: 'Data from parent to child'}
+function Home(props){
+    var listOfNewArrivals = [];
+    var listToIterate = collectionOfProducts;
+    
+    listOfNewArrivals.push(collectionOfProducts.find(item => item.id=1))
+    listToIterate = listToIterate.shift();
+    
+    let item = 5;
+    let i=0;
+    while(i!=5) {
+        var random = Math.floor(Math.random() * collectionOfProducts.length);
+        listOfNewArrivals.push(collectionOfProducts[random]);
+        i++;
     }
+    
+    console.log(listOfNewArrivals)
 
-    render() {
-        const {data} = this.state;
+    const data = 'Data from parent to child';
 
-        return (<div><HeroHome/>
-        <section>
-        <h2>New arrivals</h2>
-        <ProductCatalogueCard dataFromParentToChild = {data}/>
-        </section>
-        </div>)
+    return (<div><HeroHome/>
+    <section>
+    <h2>New arrivals</h2>
+    <ProductCatalogueCard dataFromParentToChild = {data}/>
+    </section>
+    </div>)
         
         
     }
 
-}
+
 export default Home;
