001b58b63db9d5be9f003a27b2356bfd3355f491
gpaparaz <giorgia.paparazzo@gmail.com>
2023-03-25 18:05:22 +0100

    use className instead of class for css

--- coffeehouse/src/components/header-component.js
+++ coffeehouse/src/components/header-component.js
@@ -6,30 +6,30 @@ import * as Icon from 'react-bootstrap-icons';
 
 class Header extends React.Component {
     render() {
-      return <nav class="navbar navbar-expand-lg bg-body-tertiary header d-flex justify-content-between">
-      <div class="container-fluid">
+      return <nav className="navbar navbar-expand-lg bg-body-tertiary header d-flex justify-content-between">
+      <div className="container-fluid">
         <div>
-      <img src={logo} class="header-logo"/>
-        <a class="navbar-brand header-title" href="#">CoffeeHouse</a>
-        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarText" aria-controls="navbarText" aria-expanded="false" aria-label="Toggle navigation">
-          <span class="navbar-toggler-icon"></span>
+      <img src={logo} className="header-logo"/>
+        <a className="navbar-brand header-title" href="#">CoffeeHouse</a>
+        <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarText" aria-controls="navbarText" aria-expanded="false" aria-label="Toggle navigation">
+          <span className="navbar-toggler-icon"></span>
         </button>
         </div>
-        <div class="collapse navbar-collapse justify-content-end">
+        <div className="collapse navbar-collapse justify-content-end">
           
-          <span class="navbar-text d-flex ">
-          <ul class="navbar-nav me-auto mb-2 mb-lg-0 px-4">
-            <li class="nav-item">
-              <a class="nav-link" href="#">Products</a>
+          <span className="navbar-text d-flex ">
+          <ul className="navbar-nav me-auto mb-2 mb-lg-0 px-4">
+            <li className="nav-item">
+              <a className="nav-link" href="#">Products</a>
             </li>
-            <li class="nav-item">
-              <a class="nav-link" href="#">Recipe</a>
+            <li className="nav-item">
+              <a className="nav-link" href="#">Recipe</a>
             </li>
-            <li class="nav-item">
-              <a class="nav-link" href="#">About us</a>
+            <li className="nav-item">
+              <a className="nav-link" href="#">About us</a>
             </li>
-            <li class="nav-item">
-              <a class="nav-link" href="#">Contacts</a>
+            <li className="nav-item">
+              <a className="nav-link" href="#">Contacts</a>
             </li>
           </ul>
           <Search size={25} className="icon-secondary-color mx-2"/><Cart size={25} className="icon-secondary-color mx-2"/><Icon.Person size={25} className="icon-secondary-color mx2"/>
