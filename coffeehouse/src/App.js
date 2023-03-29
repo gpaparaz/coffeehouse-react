3293056eb9c1f09278efbaeaaee4c1776678e523
gpaparaz <giorgia.paparazzo@gmail.com>
2023-03-29 22:07:50 +0200

    minor fix

--- coffeehouse/src/App.js
+++ coffeehouse/src/App.js
@@ -2,37 +2,36 @@ import './App.css';
 import Header from './components/header-component';
 import Home from './pages/home';
 
-import MainTemplate from './components/main-template';
 import { BrowserRouter, Routes } from 'react-router-dom';
 
 
 function App() {
-//   const nav = [
-//     {url: "/", text: "Home", exact: true},
-//     {url: "/products", text: "Products", exact: false},
-//     {url: "/about-us", text: "About us", exact: true}
-// ];
-//   return (
-// <BrowserRouter>
+      //   const nav = [
+      //     {url: "/", text: "Home", exact: true},
+      //     {url: "/products", text: "Products", exact: false},
+      //     {url: "/about-us", text: "About us", exact: true}
+      // ];
+      //   return (
+      // <BrowserRouter>
 
-// <MainTemplate
-// >
+      // <MainTemplate
+      // >
 
-//     <Routes>
-//         <Route path="/" element={<Home />}/>
-//         {/* <Route path="/pokedex" element={<Pokedex />}/>
-//         <Route path="/info" element={<Info />}/>
-//         <Route path="/pokedex/:number" element={<PokemonDetail />}/> */}
-//     </Routes>
+      //     <Routes>
+      //         <Route path="/" element={<Home />}/>
+      //         {/* <Route path="/pokedex" element={<Pokedex />}/>
+      //         <Route path="/info" element={<Info />}/>
+      //         <Route path="/pokedex/:number" element={<PokemonDetail />}/> */}
+      //     </Routes>
 
-// </MainTemplate>
+      // </MainTemplate>
 
-// </BrowserRouter>
+      // </BrowserRouter>
 
-return <div>
-      <Header></Header>
-      <Home></Home></div>
-  
+      return <div>
+            <Header></Header>
+            <Home></Home></div>
+
 }
 
 export default App;
--- coffeehouse/src/components/header-component.js
+++ coffeehouse/src/components/header-component.js
@@ -5,39 +5,39 @@ import Search from 'react-bootstrap-icons/dist/icons/search';
 import * as Icon from 'react-bootstrap-icons';
 
 class Header extends React.Component {
-    render() {
-      return <nav className="navbar navbar-expand-lg bg-body-tertiary header d-flex justify-content-between">
-      <div className="container-fluid">
+  render() {
+    return <nav className="navbar navbar-expand-lg bg-body-tertiary header d-flex justify-content-between">
+      <div className="container">
         <div>
-      <img src={logo} className="header-logo"/>
-        <a className="navbar-brand header-title" href="#">CoffeeHouse</a>
-        <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarText" aria-controls="navbarText" aria-expanded="false" aria-label="Toggle navigation">
-          <span className="navbar-toggler-icon"></span>
-        </button>
+          <img src={ logo } className="header-logo" />
+          <a className="navbar-brand header-title" href="#">CoffeeHouse</a>
+          <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarText" aria-controls="navbarText" aria-expanded="false" aria-label="Toggle navigation">
+            <span className="navbar-toggler-icon"></span>
+          </button>
         </div>
         <div className="collapse navbar-collapse justify-content-end">
-          
+
           <span className="navbar-text d-flex ">
-          <ul className="navbar-nav me-auto mb-2 mb-lg-0 px-4">
-            <li className="nav-item">
-              <a className="nav-link" href="#">Products</a>
-            </li>
-            <li className="nav-item">
-              <a className="nav-link" href="#">Recipe</a>
-            </li>
-            <li className="nav-item">
-              <a className="nav-link" href="#">About us</a>
-            </li>
-            <li className="nav-item">
-              <a className="nav-link" href="#">Contacts</a>
-            </li>
-          </ul>
-          <Search size={25} className="icon-secondary-color mx-2"/><Cart size={25} className="icon-secondary-color mx-2"/><Icon.Person size={25} className="icon-secondary-color mx2"/>
+            <ul className="navbar-nav me-auto mb-2 mb-lg-0 px-4">
+              <li className="nav-item">
+                <a className="nav-link" href="#">Products</a>
+              </li>
+              <li className="nav-item">
+                <a className="nav-link" href="#">Recipe</a>
+              </li>
+              <li className="nav-item">
+                <a className="nav-link" href="#">About us</a>
+              </li>
+              <li className="nav-item">
+                <a className="nav-link" href="#">Contacts</a>
+              </li>
+            </ul>
+            <Search size={ 25 } className="icon-secondary-color mx-2" /><Cart size={ 25 } className="icon-secondary-color mx-2" /><Icon.Person size={ 25 } className="icon-secondary-color mx2" />
           </span>
         </div>
       </div>
     </nav>
-    }
-    
+  }
+
 }
 export default Header;
